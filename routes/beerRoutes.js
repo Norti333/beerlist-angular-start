5eacb5a6a056a8fb572b5afa8383566d6a2cf40a
Norti333 <michael.glicks1991@gmail.com>
2017-07-11 14:14:11 +0300

    added router + start of users/login

---
+++ routes/beerRoutes.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var router = express.Router();
+var Beer = require("../models/BeerModel");
+
+router.post('/:id/ratings', function (req, res, next) {
+  var updateObject = {
+    $push: {
+      ratings: req.body.rating
+    }
+  };
+  Beer.findByIdAndUpdate(req.params.id, updateObject, {
+    new: true
+  }, function (err, beer) {
+    if (err) {
+      return next(err);
+    } else {
+      res.send(beer);
+    }
+  });
+});
+router.get('/', function (req, res, next) {
+  Beer.find(function (err, beer) {
+    if (err) {
+      return next(err);
+    }
+    for (var x = 0; x < beer.length; x++) {
+      var total = 0;
+      var length = beer[x].ratings.length
+      for (var i = 0; i < length; i++) {
+        total = total + beer[x].ratings[i]
+      }
+      var calcAvg = total / length;
+      var avg = calcAvg.toFixed(2);
+      beer[x].avg = avg
+    }
+    res.send(beer)
+  })
+});
+router.get('/:id', function (req, res, next) {
+  Beer.findById(req.params.id, function (err, beer) {
+    if (err) {
+      return next(err);
+    }
+    res.send(beer)
+  })
+});
+router.post('/', function (req, res, next) {
+  Beer.create(req.body, handler(res, next));
+});
+router.delete('/:beerId', function (req, res, next) {
+  Beer.findByIdAndRemove(req.params.beerId, handler(res, next));
+});
+router.put('/:id', function (req, res, next) {
+  Beer.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  }, function (error, beer) {
+    if (error) {
+      return next(error);
+    }
+    res.send(beer);
+  });
+});
+router.post('/:id/reviews', function (req, res, next) {
+  var update = {
+    $push: {
+      reviews: req.body
+    }
+  };
+  Beer.findByIdAndUpdate(req.params.id, update, {
+    new: true
+  }, function (err, beer) {
+    if (err) {
+      return next(err);
+    }
+    res.send(beer)
+  });
+});
+router.delete('/:beerid/reviews/:reviewid', function (req, res, next) {
+  var update = {
+    $pull: {
+      reviews: {
+        _id: req.params.reviewid
+      }
+    }
+  };
+  Beer.findByIdAndUpdate(req.params.beerid, update, {
+    new: true
+  }, function (err, beer) {
+    if (err) {
+      return next(err);
+    }
+    res.send(beer)
+  });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
-
+var beerRoutes = require('./routes/beerRoutes');
+var User = require("./models/userModel");
 mongoose.connect('mongodb://localhost/beers');
-
 var app = express();
-var Beer = require("./models/beerModel");
+
+app.use(expressSession({
+  secret: 'yourSecretHere',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
@@ -13,6 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 var handler = function (res, next) {
   return function (err, beer) {
@@ -22,113 +36,10 @@ var handler = function (res, next) {
     res.send(beer);
   }
 }
-
-app.post('/beers/:id/ratings', function (req, res, next) {
-  var updateObject = {
-    $push: {
-      ratings: req.body.rating
-    }
-  };
-
-  Beer.findByIdAndUpdate(req.params.id, updateObject, {
-    new: true
-  }, function (err, beer) {
-    if (err) {
-      return next(err);
-    } else {
-      res.send(beer);
-    }
-  });
-});
-
-
-app.get('/beers', function (req, res, next) {
-  Beer.find(function (err, beer) {
-    if (err) {
-      return next(err);
-    }
-    for (var x = 0; x < beer.length; x++) {
-      var total = 0;
-      var length = beer[x].ratings.length
-      for (var i = 0; i < length; i++) {
-        total = total + beer[x].ratings[i]
-      }
-      var calcAvg = total / length;
-      var avg = calcAvg.toFixed(2);
-      beer[x].avg = avg
-    }
-    res.send(beer)
-  })
-});
-
-app.post('/beers', function (req, res, next) {
-  Beer.create(req.body, handler(res, next));
-});
-
-app.delete('/beers/:beerId', function (req, res, next) {
-  Beer.findByIdAndRemove(req.params.beerId, handler(res, next));
-});
-
-app.put('/beers/:id', function (req, res, next) {
-  Beer.findByIdAndUpdate(req.params.id, req.body, {
-    new: true
-  }, function (error, beer) {
-    if (error) {
-      return next(error);
-    }
-    res.send(beer);
-  });
-});
-
-
-app.post('/beers/:id/reviews', function (req, res, next) {
-  var update = {
-    $push: {
-      reviews: req.body
-    }
-  };
-
-  Beer.findByIdAndUpdate(req.params.id, update, {
-    new: true
-  }, function (err, beer) {
-    if (err) {
-      return next(err);
-    }
-    res.send(beer)
-  });
-});
-
-app.delete('/beers/:beerid/reviews/:reviewid', function (req, res, next) {
-  var update = {
-    $pull: {
-      reviews: {
-        _id: req.params.reviewid
-      }
-    }
-  };
-  Beer.findByIdAndUpdate(req.params.beerid, update, {
-    new: true
-  }, function (err, beer) {
-    if (err) {
-      return next(err);
-    }
-    res.send(beer)
-  });
-});
-
-app.get('/beers/:id', function (req, res, next) {
-  Beer.findById(req.params.id, function (err, beer) {
-    if (err) {
-      return next(err);
-    }
-    res.send(beer)
-  })
-});
-
+app.use('/beers', beerRoutes);
 app.all('*', function (req, res) {
   res.sendFile(__dirname + "/public/index.html")
 })
-
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -143,7 +54,6 @@ app.use(function (err, req, res, next) {
     error: err
   });
 });
-
 app.listen(8888, function () {
   console.log("yo yo yo, on 8888!!")
 });
